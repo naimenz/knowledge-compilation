4146456359aa2c824a2baa4f85d512279399bd27
naimenz <ianmck98@gmail.com>
2021-06-01 12:21:08 +0100

    Added constrained clauses

--- data_structures/clauses.py
+++ data_structures/clauses.py
@@ -5,6 +5,7 @@ This includes constrained AND unconstrained clauses.
 
 from logicalterms import *
 from literals import *
+from constraintsets import *
 from abc import ABC
 
 class Clause(ABC):
@@ -22,6 +23,27 @@ class UnconstrainedClause(Clause):
         literal_strs = [str(literal) for literal in self.literals]
         return f"({' OR '.join(literal_strs)})"
 
+
+class ConstrainedClause(Clause):
+    """An FOL-DC constrained clause.
+    This consists of an unconstrained clause with a set of bound variables and a constraint set.
+
+    NOTE: For now we only work with bound *logical* variables."""
+
+    def __init__(self,
+            unconstrained_clause: 'UnconstrainedClause',
+            bound_vars: list['LogicalVariable'],
+            cs: 'ConstraintSet'):
+        self.unconstrained_clause = unconstrained_clause
+        self.bound_vars = bound_vars
+        self.cs = cs
+
+    def __str__(self) -> str:
+        bound_vars_strs = [str(var) for var in self.bound_vars]
+        return f"FORALL {{{', '.join(bound_vars_strs)}}}, {self.cs} : {self.unconstrained_clause}"
+
+
+
 if __name__ == '__main__':
     pred1 = Predicate('smokes', 1)
     pred2 = Predicate('friends', 2)
@@ -34,8 +56,29 @@ if __name__ == '__main__':
     literal1 = Literal(atom1, True)
     literal2 = Literal(atom2, False)
 
-    clause = UnconstrainedClause([literal1, literal2])
+    uclause = UnconstrainedClause([literal1, literal2])
+
+    # v1 = LogicalVariable('X')
+    # c1 = Constant('bob')
+    c2 = Constant('a')
+    d = SetOfConstants([c1, c2])
+
+    eq_constraint = EqualityConstraint(v1, c2)
+    ineq_constraint = InequalityConstraint(v1, c2)
+    in_constraint = InclusionConstraint(c1, d)
+    notin_constraint = NotInclusionConstraint(c1, d)
+
+    cs = ConstraintSet([eq_constraint, ineq_constraint, in_constraint, notin_constraint])
+
+    v2 = LogicalVariable('Y')
+    clause = ConstrainedClause(uclause, [v1, v2], cs)
     print(clause)
+
+    
+
+
+
+
 
 
 
