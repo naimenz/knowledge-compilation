a516090af39d0c05ab2223a66b628482bf8651a5
naimenz <ianmck98@gmail.com>
2021-06-01 11:31:48 +0100

    Set up terms, constants, variables

---
+++ data_structures/logicalterms.py
@@ -0,0 +1,55 @@
+"""
+Classes for types of logical terms, which are constants and variables
+"""
+
+from abc import ABC, abstractmethod
+
+class LogicalTerm(ABC):
+    """
+    An abstract base class for logical terms.
+    Terms are either constants or variables, so can never be instantiated directly.
+    """
+
+
+class Constant(LogicalTerm):
+    """
+    A FOL constant. 
+    """
+    def __init__(self, value: str) -> None:
+        self._value = value
+
+    @property
+    def value(self) -> str:
+        """We use a property because the value of a constant should
+        never be changed once set."""
+        return self._value
+
+
+    def __str__(self) -> str:
+        return f'{self.value}'
+
+
+
+class LogicalVariable(LogicalTerm):
+    """
+    A FOL logical variable.
+    """
+    def __init__(self, symbol: str) -> None:
+        self.symbol = symbol
+
+    def __str__(self) -> str:
+        return f'{self.symbol}'
+
+
+
+if __name__ == '__main__':
+    c = Constant('a')
+    print(c.value)
+    print(c)
+
+    x = LogicalVariable('X')
+    print(x.symbol)
+    print(x)
+
+
+
