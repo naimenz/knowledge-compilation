3ae6a41c429de4207e8409e2507e349035049aed
naimenz <ianmck98@gmail.com>
2021-06-08 12:25:45 +0100

    Changed substitution to accept variables as well as constants

--- kc/data_structures/substitutions.py
+++ kc/data_structures/substitutions.py
@@ -1,6 +1,5 @@
 """
 Classes for substitutions.
-NOTE: For now, I consider substitutions of constants to variables, not free variables to variables
 """
 from kc.data_structures.logicalterms import *
 
@@ -8,24 +7,24 @@ from typing import List, Tuple, Dict, Any
 
 class Substitution:
     """A FOL substitution.
-    This contains a dictionary of logical variables and their constants
+    This contains a dictionary of logical variables and their substitutions (which are terms)
     """
-    def __init__(self, variable_constant_pairs: List[Tuple['LogicalVariable', 'Constant']]) -> None:
+    def __init__(self, variable_term_pairs: List[Tuple['LogicalVariable', 'LogicalTerm']]) -> None:
         """The substitution dict is private and shouldn't be changed after creation"""
-        self._substitution_dict = {var: const for var, const in variable_constant_pairs}
+        self._substitution_dict = {var: term for var, term in variable_term_pairs}
 
-    def __getitem__(self, key: 'LogicalVariable') -> 'Constant':
-        """Return the constant associated with a given variable"""
+    def __getitem__(self, key: 'LogicalVariable') -> 'LogicalTerm':
+        """Return the term associated with a given variable"""
         return self._substitution_dict[key]
 
     def __eq__(self, other: Any) -> bool:
-        """Two substitutions are equal if they have all the same (variable, constant) pairs"""
+        """Two substitutions are equal if they have all the same (variable, term) pairs"""
         if not isinstance(other, Substitution):
             return False
         return self._substitution_dict == other._substitution_dict
     def __str__(self) -> str:
         rightarrow_string = ' \u2192 '
-        substitution_strings = [str(v) + rightarrow_string + str(c) for v, c in self._substitution_dict.items()]
+        substitution_strings = [str(v) + rightarrow_string + str(t) for v, t in self._substitution_dict.items()]
         return f"{{{', '.join(substitution_strings)}}}"
 
     def __repr__(self) -> str:
@@ -34,9 +33,9 @@ class Substitution:
 
 if __name__ == '__main__': 
     variables = [LogicalVariable('X'), LogicalVariable('Y'), LogicalVariable('Z')]
-    constants = [Constant('a'), Constant('b'), Constant('a')]
+    sub_constants: List['LogicalTerm'] = [Constant('a'), Constant('b'), Constant('a')]
 
-    pairs = [(v, c) for v, c in zip(variables, constants)]
+    pairs = [(v, c) for v, c in zip(variables, sub_constants)]
     substitution = Substitution(pairs)
     print(substitution)
     print(substitution[variables[0]])
