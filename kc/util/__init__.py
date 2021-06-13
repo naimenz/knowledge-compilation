0c12c400c4d534a56ba65717765a8c447a692249
naimenz <ianmck98@gmail.com>
2021-06-13 12:08:53 +0100

    Added placeholder compile_theory function

--- kc/util/__init__.py
+++ kc/util/__init__.py
@@ -1,5 +1,7 @@
+from kc.util.compile import *
 from kc.util.utility import *
 from kc.util.mgu import *
 from kc.util.splitting import *
 from kc.util.conditioning import *
 from kc.util.unitprop import *
+from kc.util.independence import *
---
+++ kc/util/compile.py
@@ -0,0 +1,10 @@
+"""File for the whole Compile algorithm.
+For now this is just a placeholder so I can put it in the various algorithms
+"""
+
+from kc.data_structures import *
+
+def compile_theory(delta: 'CNF') -> 'CNF':
+    """For now, this is just the identity
+    TODO: implement compile algorithm for real"""
+    return delta
