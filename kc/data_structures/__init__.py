8d8c0ba184a12ee597f22475cdcc38dc6c624d10
naimenz <ianmck98@gmail.com>
2021-06-14 15:33:48 +0100

    Importing only classes from data_structures

--- kc/data_structures/__init__.py
+++ kc/data_structures/__init__.py
@@ -1,8 +1,8 @@
-from kc.data_structures.logicalterms import *
-from kc.data_structures.domainterms import *
-from kc.data_structures.literals import *
-from kc.data_structures.substitutions import *
-from kc.data_structures.clauses import *
-from kc.data_structures.cnf import *
-from kc.data_structures.constraints import *
-from kc.data_structures.equivalenceclasses import *
+from kc.data_structures.logicalterms import LogicalTerm, LogicalVariable, Constant
+from kc.data_structures.domainterms import DomainTerm, SetOfConstants, DomainVariable
+from kc.data_structures.literals import Literal, Atom, GroundAtom, Predicate
+from kc.data_structures.substitutions import Substitution
+from kc.data_structures.cnf import CNF
+from kc.data_structures.clauses import Clause, UnconstrainedClause, ConstrainedClause, UnitClause, ConstrainedAtom
+from kc.data_structures.constraints import Constraint, LogicalConstraint, SetConstraint, EqualityConstraint, InequalityConstraint, InclusionConstraint, NotInclusionConstraint, ConstraintSet
+from kc.data_structures.equivalenceclasses import EquivalenceClass 
