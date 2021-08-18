33422835fa964dc2973a6eba054a7d3b1e0be803
Jonathan Feldstein <jonathan.feldstein@ed.ac.uk>
2021-08-18 16:14:27 +0100

    Checked compile

--- kc/compiler/compiler.py
+++ kc/compiler/compiler.py
@@ -10,53 +10,41 @@ from kc.compiler import Independence, ShannonDecomposition, ShatteredCompilation
 from kc.compiler import IndependentSingleGroundings, IndependentPairedGroundings, AtomCounting
 from kc.compiler import CheckTautology, Ground
 
-# NOTE DEBUG: Limiting the number of recursions for debugging
-import sys
-sys.setrecursionlimit(100) 
-
 from typing import Dict, Optional, Tuple, Any, Type
 
 
 class Compiler:
     """A knowledge compilation compiler that takes CNFs and produces 
-    FO-da-DNNF (or FO-sda-DNNF) circuits, following VdB's PhD."""
+    FO-da-DNNF circuits, following VdB's PhD."""
 
     def __init__(self) -> None:
-        """We initialise the compiler with an empty cache of previously-seen theories
+        """We initialise the compiler with an empty cache for previously-seen theories
         and a list of the compilation rules that can be applied"""
         self._cache: Dict['CNF', 'NNFNode'] = dict()
         self.rules: Tuple[Type['KCRule'], ...] = (LeafConstruction,
-                                       UnitPropagation,
-                                       VacuousConjunction,
-                                       Independence,
-                                       ShannonDecomposition,
-                                       ShatteredCompilation,  
-                                       IndependentSingleGroundings,
-                                       IndependentPairedGroundings,
-                                       CheckTautology,
-                                       AtomCounting,
-                                       Ground)
+                                                  UnitPropagation,
+                                                  VacuousConjunction,
+                                                  Independence,
+                                                  ShannonDecomposition,
+                                                  ShatteredCompilation,
+                                                  IndependentSingleGroundings,
+                                                  IndependentPairedGroundings,
+                                                  CheckTautology,
+                                                  AtomCounting,
+                                                  Ground)
 
     def compile(self, theory: 'CNF') -> 'NNFNode':
         """This function follows closely the algorithm described in the PhD and 
         the one used in Forclift"""
-        # ensuring that ranges are subdivided before continuing
+        # ensuring that ranges of SMT atoms are subdivided before continuing
         if not theory.subdivided: 
             theory = theory.subdivide_ranges()
-        # if there are no clauses in the theory, then nothing to do
-        #  TODO: make this nicer
-        if len(theory.clauses) == 0:
-            print("EMPTY HERE")
-            return EmptyNode()
 
         # if self.cache_contains(theory):
         #     print(f"DEBUG: Hit cache")
         #     return self.get_cache(theory)
 
-        nnf: Optional['NNFNode'] = None
-
-        # NOTE TODO: Check if this is breaking things
-        # first we rename the variables in all the clauses to be different
+        # first we rename the variables in all the clauses to be different to simplify mgu finding later in the code
         theory = theory.make_variables_different()
 
         applicable_rule: Optional[Type['KCRule']]
@@ -68,13 +56,12 @@ class Compiler:
 
         if applicable_rule is None:
             raise ValueError("Compilation failed - no rule found for {theory}")
-            nnf = None
         else:
-            nnf = self.apply_rule(theory, applicable_rule, stored_data) 
-        self.set_cache(theory, nnf)
-        return nnf
+            nnf_node = self.apply_rule(theory, applicable_rule, stored_data)
+        self.set_cache(theory, nnf_node)
+        return nnf_node
 
-    def cache_contains(self, cnf: 'CNF') -> bool:
+    def cache_contains_cnf(self, cnf: 'CNF') -> bool:
         """Check if the cache contains a given CNF"""
         return cnf in self._cache.keys()
 
@@ -88,7 +75,7 @@ class Compiler:
     def set_cache(self, cnf: Optional['CNF'], nnf: 'NNFNode') -> None:
         """Set the cache value for a given cnf if that cnf compiled AND we haven't
         seen it before (because they should always compile to the same thing)"""
-        if not (cnf is None or self.cache_contains(cnf)):
+        if not (cnf is None or self.cache_contains_cnf(cnf)):
             self._cache[cnf] = nnf
 
     def find_rule(self, theory: 'CNF') -> Tuple[Optional[Type['KCRule']], Optional[Any]]:
@@ -120,5 +107,5 @@ class Compiler:
         """Apply a given compilation rule to a cnf and return the constructed NNF
         NOTE: we also accept precomputed data from find_rule and pass it on if it's not None"""
         # we pass this compiler in so it can be called recursively
-        nnf = rule.apply(theory, stored_data, self)
-        return nnf
+        nnf_node = rule.apply(theory, stored_data, self)
+        return nnf_node
